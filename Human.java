1367bb088e3e562b7c85fb23c9b02a9d23b9900b
Mihslo <101973450+Mihslo@users.noreply.github.com>
2022-04-25 22:29:28 +0300

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,26 @@
+package gb.HomeWorkApp4;
+
+public class Human implements Units{
+    public int runLenght;
+    public int jumpHight;
+    public String name;
+
+    public Human(String name,int unLenght, int jumpHight) {
+        this.runLenght = run();
+        this.jumpHight = jump();
+        this.name = name;
+    }
+@Override
+    public int run() {
+        runLenght = (int) (Math.random() * 30);
+        return runLenght;
+    }
+@Override
+    public int jump() {
+        jumpHight = (int) (Math.random() * 5);
+        return jumpHight;
+    }
+    public String getName(){
+        return  this.name;
+    }
+}
